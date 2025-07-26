3de031935185d33f6e87b0028f354f80d86b3102
yuvraj-6353 <yuviraj1107@gmail.com>
2025-07-26 14:50:35 +0530

    heroindustrial to herosection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,10 +1,8 @@
-
 import heroImage from "../assets/hero-image.jpeg";
 
-const HeroIndustrial = () => {
+const HeroSection = () => {
   return (
     <>
-      
       <section
         className="relative w-full"
         style={{ height: "calc(100vh - 80px)", marginTop: "80px" }}
@@ -71,4 +69,4 @@ const HeroIndustrial = () => {
   );
 };
 
-export default HeroIndustrial;
+export default HeroSection;
